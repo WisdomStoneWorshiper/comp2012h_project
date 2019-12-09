546085542b2ffe7509208e25cc4948fb9daf37e4
TatatWong <tatwong4@gmail.com>
2019-12-09 17:57:25 +0800

    update email staff

--- emailDeck.cpp
+++ emailDeck.cpp
@@ -4,22 +4,38 @@ EmailDeck::EmailDeck(){
 
         for(int i =1; i<=4 ; ++i){
             EmailFly * p = new EmailFly(i);
-            cardDeck.push_back(p);
+            deck.push_back(p);
         }
 
-        for(int i =1; i<=4 ; ++i){
+        for(int i =1; i<=6 ; ++i){
             EmailMoney * p = new EmailMoney(i);
-            cardDeck.push_back(p);
+            deck.push_back(p);
         }
 
-        EmailJailPass * p = new EmailJailPass;
-        cardDeck.push_back(p);
+        deck.push_back(new EmailJailPass);
 
-        std::random_shuffle ( cardDeck.begin(), cardDeck.end() );
+        emailOrder = deck.begin();
 
+
+}
+
+void EmailDeck::shuffle(){ //shuffle before isEmpty function
+     std::random_shuffle ( deck.begin(), deck.end() );
 }
 
 
 
 
+bool EmailDeck::isEmpty(){
+    return emailOrder == deck.begin();
+}
 
+Email * EmailDeck ::getEmail(){
+
+
+    deque<Email*>:: iterator temp;
+    temp = emailOrder;
+    emailOrder ++;
+    return *temp;
+
+}
